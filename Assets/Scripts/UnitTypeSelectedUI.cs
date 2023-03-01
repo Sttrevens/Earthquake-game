91554ffd1eda57433e0a17463550fe2b6bcb6cfb
Sttrevens <Sttrevens@126.com>
2023-02-28 23:59:25 -0500

    Current Version

--- Assets/Scripts/UnitTypeSelectedUI.cs
+++ Assets/Scripts/UnitTypeSelectedUI.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static System.Net.Mime.MediaTypeNames;
 
 public class UnitTypeSelectedUI : MonoBehaviour
 {
